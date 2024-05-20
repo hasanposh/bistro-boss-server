51a1b865166df515cf8414b406f1dd4c452cb4f1
hasanposh <hmasif147@gmail.com>
2024-05-21 03:25:36 +0600

    cart section done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlu13v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,17 +24,40 @@ async function run() {
   try {
     const menuCollection = client.db("bistroBossDB").collection("menu");
     const reviewsCollection = client.db("bistroBossDB").collection("reviews");
-
+    const cartsCollection = client.db("bistroBossDB").collection("carts");
+    // menuCollection
     app.get("/menu", async (req, res) => {
-        const cursor = menuCollection.find();
+      const cursor = menuCollection.find();
       const results = await cursor.toArray();
       res.send(results);
     });
+    // review collection
     app.get("/reviews", async (req, res) => {
-        const cursor = reviewsCollection.find();
+      const cursor = reviewsCollection.find();
       const results = await cursor.toArray();
       res.send(results);
     });
+    // cart collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = cartsCollection.find(query);
+      const results = await cursor.toArray();
+      res.send(results);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.json(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.json(result);
+    })
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
